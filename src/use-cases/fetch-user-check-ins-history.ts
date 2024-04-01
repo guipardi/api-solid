21f03626949c868feea1c96b6958e29319003be5
Guilherme Pardi <guilhermepardi@gmail.com>
2024-04-01 19:04:41 -0300

    feat: Create 'findManyByUserId' method in checkInsRepository; create and test 'fetch-user-check-ins' history use case

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-respository'
+
+interface FetchCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchCheckInsHistoryUseCaseRequest): Promise<FetchCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
