da7dc948c0d712f37d5d0eed14b4ea5e8295dfc3
Guilherme Pardi <guilhermepardi@gmail.com>
2024-04-04 08:57:30 -0300

    feat: Creating prisma-check-ins-repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,10 +3,14 @@ import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findById() {
-    throw new Error('Method not implemented.')
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
 
-    return null
+    return user
   }
 
   async findByEmail(email: string) {
