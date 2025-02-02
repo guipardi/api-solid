4a608fc42336054c8044bd376df9bea608bd360c
Guilherme Pardi <guilhermepardi@gmail.com>
2025-02-02 12:28:46 -0300

    feat: using use-cases and repository pattern

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,23 @@
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
+import { hash } from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+import { registerService } from "@/services/register";
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    await registerService({ name, email, password });
+  } catch (err) {
+    return reply.status(409).send();
+  }
+
+  return reply.status(201).send();
+}
